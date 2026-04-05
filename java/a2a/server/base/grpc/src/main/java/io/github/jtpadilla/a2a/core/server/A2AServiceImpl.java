7d7136324b8fa95b5aed1dd455af23bf311faa37
padilla <padilla@speedycontrol.com>
2026-04-05 19:46:49 +0200

    Implementando A2A

--- java/a2a/server/base/grpc/src/main/java/io/github/jtpadilla/a2a/core/server/A2AServiceImpl.java
+++ java/a2a/server/base/grpc/src/main/java/io/github/jtpadilla/a2a/core/server/A2AServiceImpl.java
@@ -3,9 +3,11 @@ package io.github.jtpadilla.a2a.core.server;
 import com.google.lf.a2a.v1.*;
 import io.github.jtpadilla.a2a.server.service.agentcard.AgentCardService;
 import io.github.jtpadilla.a2a.server.service.skill.SkillService;
+import io.github.jtpadilla.a2a.server.service.skill.spi.SkillProvider;
 import io.grpc.stub.StreamObserver;
 import io.helidon.service.registry.Service;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -14,13 +16,17 @@ public class A2AServiceImpl extends A2AServiceGrpc.A2AServiceImplBase {
 
     private static final Logger LOGGER = Logger.getLogger(A2AServiceImpl.class.getName());
 
-    private SkillService skillService;
-    private AgentCardService agentCardService;
+    final private SkillService skillService;
+    final private AgentCardService agentCardService;
 
     @Service.Inject
     public A2AServiceImpl(SkillService skillService, AgentCardService agentCardService) {
         this.skillService = skillService;
         this.agentCardService = agentCardService;
+
+        System.out.println("Skill list:");
+        final List<SkillProvider> skillProviders = skillService.skillList();
+        skillProviders.stream().forEach(System.out::println);
     }
 
     @Override
