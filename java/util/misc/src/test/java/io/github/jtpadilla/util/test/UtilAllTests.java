1b3733f7b10938ab286d8eac1ab3c0ba76ddad2e
padilla <padilla@speedycontrol.com>
2026-04-06 19:09:23 +0200

    Implementando A2A

---
+++ java/util/misc/src/test/java/io/github/jtpadilla/util/test/UtilAllTests.java
@@ -0,0 +1,11 @@
+package io.github.jtpadilla.util.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ByteUtilsTest.class
+})
+public class UtilAllTests {
+}
