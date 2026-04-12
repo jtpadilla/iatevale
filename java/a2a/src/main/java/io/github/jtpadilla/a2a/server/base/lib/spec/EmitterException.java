2501d843eede5025f6a4b297e33967a97280b9c3
padilla <padilla@uji.es>
2026-04-12 20:07:49 +0200

    Implementando a2a

--- java/a2a/src/main/java/io/github/jtpadilla/a2a/server/base/lib/spec/EmitterException.java
+++
@@ -1,9 +0,0 @@
-package io.github.jtpadilla.a2a.server.base.lib.spec;
-
-public class EmitterException extends Exception {
-
-    public EmitterException(String message) {
-        super(message);
-    }
-
-}
